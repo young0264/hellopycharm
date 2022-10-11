013f4e21daf45204e85d467e4955e44941cfa9ec
남의영 <ny2485@naver.com>
2022-10-11 10:48:41 +0900

    0만들기 _ 백준_골5_구현백트

--- 백준/7490_0만들기.py
+++ 백준/7490_0만들기.py
@@ -1,13 +1,65 @@
 t = int(input())
-arr = ['','+','-']
+arr = [' ', '+', '-']
+
+
+def cirNum(num):
+    res = int(num[0])
+    for i in range(1, 2 * n - 1, 2):
+        if num[i] == ' ':
+            print(num[i-1:i+2])
+            print(num[i-1]+num[i+1])
+            r = num[i-1]+num[i+1]
+            num[i-1:i+2] = r
+
+
+            print("n",num)
+    for i in range(1, len(num)):
+        if num[i] == '+':
+            res += int(num[i+1])
+        elif num[i] == '-':
+            res -= int(num[i+1])
+    # print("res",res)
+    if res==0:
+        return True
+    else:
+        return False
+
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+def dfs(num,box):
+    if num==n+1:
+        new_box = box.replace(' ','')
+        r = eval(new_box)
+        if r==0:
+            answer.append(box)
+        return
+    for a in arr:
+        dfs(num+1, box+a+str(num))
+
+answer = []
 for _ in range(t):
     n = int(input())
-    box = []
+    dfs(2,'1')
+    answer.append(' ')
+print(*answer,sep='\n')
 
-
-def dfs(num,cnt,value):#cnt 0부터
-    if cnt == n:
-        if value==0:
-            print(''.join(box))
-    box.append(num)
-    for i in range(3):
+# def dfs(num):  # cnt 0부터
+#     if len(box)>=2*n+1:
+#         return
+#     if num==n+1:
+#     # if len(box)==2*n-1:
+#         print("box",box)
+#         return
+#
+#     box.append(num)
+#     for i in range(3):
+#         box.append(arr[i])
+#         dfs(num + 1)
+#         box.pop()
+#
+# for _ in range(t):
+#     n = int(input())
+#     box = []
+#     dfs(1)
+#
