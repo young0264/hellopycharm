8a12efaa4f272024d0a9169858860d4f46db5d3e
남의영 <ny2485@naver.com>
2024-08-09 23:08:01 +0900

    fix num2 exam

--- 익후/num2.py
+++ 익후/num2.py
@@ -1,38 +1,17 @@
-n = 111 # n번째
-
-length = 1 # 자릿 수
-endOfNum = 9 # 자릿수에 해당하는 숫자 수 #9,90,900,....
-totalLength = endOfNum*length
-
-startOfNum = 1 # 자릿수에 해당하는 숫자 시작점 #1,10,100....
-
-
-while(n > length * endOfNum):
-# while(n > totalLength):
+n = 11
+length = 1  # 자릿 수
+endOfNum = 9  # 자릿수에 해당하는 숫자 수 #9,90,900,....
+startOfNum = 1  # 자릿수에 해당하는 숫자 시작점 #1,10,100....
+while (n > length * endOfNum):
     print(length * endOfNum)
-    # print(totalLength)
-    n -= length * endOfNum # 덩어리치기
-    # n -= totalLength
+    n -= length * endOfNum  # 덩어리치기
+
     endOfNum *= 10
     startOfNum *= 10
     length += 1
+res = (n - 1) // length  # 뒤에 붙는 자릿수 나머지 숫자
+idx = (n - 1) % length
+expectLengthNum = (startOfNum + res)
 
-# 10~99
-# 100~999
-print("n:",n)
-
-# startOfNum += (n - 1) // length
-# s = str(startOfNum)
-
-res = startOfNum + n
-print("res:",res)
-# print("s:",s)
-# print("res:",int(s[(n - 1) % length]))
-
-# print(totalLength)
-# print(length * endOfNum)
-# print(startOfNum)
-
-
-
-
+answer = str(expectLengthNum)[idx]
+print("answer:", answer)
